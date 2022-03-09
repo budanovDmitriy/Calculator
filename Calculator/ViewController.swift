b40911490abccd4f2b4f04f4a85d20bd2043bf5f
budanovDmitriy <lllytnick1@gmail.com>
2022-03-09 14:39:27 +0300

    first commit

---
+++ Calculator/ViewController.swift
@@ -0,0 +1,96 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Dmitriy Budanov on 09.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var resultLabel: UILabel!
+    private var firstNumber: Double = 0.0
+    private var symbol: String = ""
+    private var isSymbolPressed: Bool = false
+    
+    @IBOutlet var buttons: [UIButton]!
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+    return .lightContent
+    }
+    
+    @IBAction func pressNumbers(_ sender: UIButton) {
+        let buttonText = sender.titleLabel?.text!
+        let labelText = resultLabel.text!
+        if buttonText == "." && labelText.contains(".") {
+            return
+        }
+        if isSymbolPressed {
+            resultLabel.text = buttonText
+            isSymbolPressed = false
+            return
+        }
+        resultLabel.text =
+                "\((Double(labelText) != 0.0 || labelText.contains(".") || buttonText == ".") ? labelText : "")" +
+                        "\(buttonText!)"
+    }
+    
+    @IBAction func pressSymbol(_ sender: UIButton) {
+        firstNumber = Double(resultLabel.text!)!
+        symbol = (sender.titleLabel?.text)!
+        isSymbolPressed = true
+    }
+    
+    @IBAction func pressReset(_ sender: UIButton) {
+        resultLabel.text = String(0)
+        firstNumber = 0.0
+        symbol = ""
+        isSymbolPressed = false
+    }
+    
+    @IBAction func pressCalculate(_ sender: UIButton) {
+        let secondNumber = Double(resultLabel.text!)!
+        let result = calculate(secondNumber)
+        resultLabel.text = String(result)
+        firstNumber = 0.0
+        symbol = ""
+        isSymbolPressed = true
+    }
+    
+    @IBAction func pressNegative(_ sender: UIButton) {
+        resultLabel.text = String(Double(resultLabel.text!)! * (-1))
+        isSymbolPressed = true
+    }
+    
+    private func calculate(_ secondNumber: Double) -> Double {
+        switch symbol {
+        case "+": return firstNumber + secondNumber
+        case "-": return firstNumber - secondNumber
+        case "×": return firstNumber * secondNumber
+        case "÷": return firstNumber / secondNumber
+        case "%": return firstNumber * secondNumber / 100
+        default: return Double(resultLabel.text!)!
+        }
+    }
+    
+    private func setupButtons(){
+        buttons.forEach(){ button in
+            button.layoutIfNeeded()
+            button.layer.cornerRadius = button.frame.height / 2
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        setupButtons()
+    }
+    
+    
+
+}
+
